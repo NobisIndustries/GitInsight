18881e1cc0374c4ff5f6b8cef85ef9ad47c0237f
Fabian <fabiannobis@gmx.de>
2020-11-10 23:39:57 +0100

    Group plot by teams

--- frontend/src/components/pathAnalysis/CommitPlot.vue
+++ frontend/src/components/pathAnalysis/CommitPlot.vue
@@ -11,7 +11,6 @@
 
 <script>
   import { Plotly } from 'vue-plotly';
-  import DataFrame from "dataframe-js";
 
   export default {
     name: 'CommitPlot',
@@ -20,7 +19,12 @@
     },
     data() {
       return {
-        layout: {}
+        layout: {
+          hovermode: 'closest',
+          yaxis: {
+            automargin: true,
+          }
+        }
       };
     },
     methods: {
@@ -32,26 +36,38 @@
       plot_data() {
         let entry_history = this.$store.state.current_entry_history;
         if (!entry_history)
-          return {};
+          return [];
 
         const min_size = 2;
         const max_size = 25;
         entry_history = entry_history.withColumn('scale',
             row => min_size + (max_size - min_size) / (Math.pow(row.get('number_affected_files'), 0.5)));
         entry_history = entry_history.withColumn('description',
-            row => `${row.get('author')}<br>${row.get('authored_date_time')}<br>------------<br>${row.get('message')}`.replace('\n', '<br>'));
+            row => (`${row.get('author')}<br>${row.get('team_display_name')}<br>`
+                         + `${row.get('authored_date_time')}<br>------------<br>`
+                         + `${row.get('message')}`.replace('\n', '<br>')));
 
-        let plot_data = [{
-              x: entry_history.toArray('authored_date_time'),
-              y: entry_history.toArray('author'),
-              marker: {
-                size: entry_history.toArray('scale'),
-              },
-              hoverinfo: 'text',
-              hovertext: entry_history.toArray('description'),
-              type: "scatter",
-              mode: 'markers',
-            }];
+        let plot_data = [];
+
+        let entries_by_teams = entry_history.groupBy('team_display_name');
+        let groupKey, group;
+        for({groupKey, group} of entries_by_teams) {
+          plot_data.push(
+              {
+                x: group.toArray('authored_date_time'),
+                y: group.toArray('author'),
+                marker: {
+                  size: group.toArray('scale'),
+                  color: group.toArray('team_display_color'),
+                },
+                hoverinfo: 'text',
+                hovertext: group.toArray('description'),
+                type: "scatter",
+                mode: 'markers',
+                name: groupKey['team_display_name'],
+              }
+          );
+        }
         return plot_data;
       }
     }
