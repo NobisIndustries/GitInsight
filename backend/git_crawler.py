38c28a63fbcc5d50a830fa2da0c9498d99b95cc0
Fabian <fabiannobis@gmx.de>
2020-11-14 18:30:57 +0100

    Clean up the crawler a bit:
    - CommitFetcher -> CommitProvider
    - Commit members now public
    - Correct type hint in __process_child_commit
    - Auto format

--- backend/git_crawler.py
+++ backend/git_crawler.py
@@ -1,7 +1,6 @@
 import pprint
 from collections import defaultdict
 from copy import copy
-from dataclasses import dataclass
 from pathlib import Path, PurePath
 from typing import Dict, List
 from uuid import uuid4
@@ -33,22 +32,24 @@ class Commit:
         )
 
     def __init__(self, hash: str, metadata, affected_files, already_in_db: bool):
-        self._hash = hash
-        self._metadata = metadata
-        self._affected_files = affected_files
+        self.hash = hash
+        self.metadata = metadata
+        self.affected_files = affected_files
+
         self._already_in_db = already_in_db
-    
+
     def add_to_db(self, db_session):
         if self._already_in_db:
             return
-        sql_metadata = self._metadata.to_db_representation(self._hash, self._affected_files.get_number_affected_files())
+        sql_metadata = self.metadata.to_db_representation(self.hash, self.affected_files.get_number_affected_files())
         db_session.add(sql_metadata)
 
-        for sql_affected_file in self._affected_files.to_db_representation(self._hash):
+        for sql_affected_file in self.affected_files.to_db_representation(self.hash):
             db_session.add(sql_affected_file)
 
     def __repr__(self):
-        return pprint.pformat([f'hash: {self._hash}', f'metadata: {self._metadata}', f'affected_files: {self._affected_files}'])
+        return '\n    '.join(['', f'hash: {self.hash}', f'metadata: {self.metadata}',
+                              f'affected_files: {self.affected_files}'])
 
 
 class CommitMetadata:
@@ -83,12 +84,13 @@ class CommitMetadata:
         )
 
     def __repr__(self):
-        return ', '.join([f'authored_timestamp: {self._authored_timestamp}', f'author: {self._author}', f'message: {self._message.strip()}'])
+        return ', '.join([f'authored_timestamp: {self._authored_timestamp}', f'author: {self._author}',
+                          f'message: {self._message.strip()}'])
 
 
 class CommitAffectedFiles:
     @classmethod
-    def from_git_commit(cls, git_commit: git.Commit): 
+    def from_git_commit(cls, git_commit: git.Commit):
         diffs = git_commit.parents[0].diff(git_commit.hexsha) if git_commit.parents else git_commit.diff(git.NULL_TREE)
         affected_files = [CommitAffectedFile.from_file_diff(diff) for diff in diffs]
         return CommitAffectedFiles(affected_files)
@@ -100,7 +102,7 @@ class CommitAffectedFiles:
 
     def __init__(self, affected_files):
         self._affected_files = affected_files
-    
+
     def get_number_affected_files(self):
         return len(self._affected_files)
 
@@ -110,7 +112,7 @@ class CommitAffectedFiles:
     def __iter__(self):
         for affected_file in self._affected_files:
             yield affected_file
-    
+
     def __repr__(self):
         return pprint.pformat(self._affected_files)
 
@@ -142,37 +144,38 @@ class CommitAffectedFile:
         self.new_path = new_path
         self.change_type = change_type
         self.file_id = file_id
-    
+
     def to_db_representation(self, hash):
         return db.SqlAffectedFile(
-            hash=hash, 
+            hash=hash,
             file_id=self.file_id,
             old_path=self.old_path,
             new_path=self.new_path,
             change_type=self.change_type
         )
-    
+
     def __repr__(self):
-        return ', '.join([f'old_path: {self.old_path}', f'new_path: {self.new_path}', f'change_type: {self.change_type}', f'file_id: {self.file_id}'])
+        return ', '.join([f'old_path: {self.old_path}', f'new_path: {self.new_path}',
+                          f'change_type: {self.change_type}', f'file_id: {self.file_id}'])
 
 
 class CurrentFilePaths:
     def __init__(self):
         self._current_paths_of_branches = {}
-    
+
     def add_branch_paths(self, branch, current_paths_of_branch):
         self._current_paths_of_branches[branch] = copy(current_paths_of_branch)
-    
+
     def add_to_db(self, db_session):
         for branch, entries in self._current_paths_of_branches.items():
             for file_id, current_path in entries.items():
                 sql_entry = db.SqlCurrentFilePath(
-                     branch=branch,
-                     file_id=file_id,
-                     current_path=current_path
+                    branch=branch,
+                    file_id=file_id,
+                    current_path=current_path
                 )
                 db_session.add(sql_entry)
-    
+
     def __repr__(self):
         return pprint.pformat(self._current_paths_of_branches)
 
@@ -181,7 +184,7 @@ class CrawlResult:
     def __init__(self, child_commit_tree: Dict[str, List[Commit]], current_paths_of_branches: CurrentFilePaths):
         self.child_commit_tree = child_commit_tree
         self.current_paths_of_branches = current_paths_of_branches
-    
+
     def write_to_db(self):
         db_engine = db.get_engine()
 
@@ -199,7 +202,7 @@ class CrawlResult:
         db_session.commit()
 
 
-class CommitFetcher:
+class CommitProvider:
     def __init__(self):
         self._available_commits = self.__fetch_all_commits_from_db()
 
@@ -213,7 +216,6 @@ class CommitFetcher:
 
         return {db_md.hash: Commit.from_db(db_md, affected_files_of_commit[db_md.hash]) for db_md in db_metadata}
 
-
     def get_commit(self, git_commit: git.Commit) -> Commit:
         if git_commit.hexsha in self._available_commits:
             return self._available_commits[git_commit.hexsha]
@@ -223,7 +225,7 @@ class CommitFetcher:
 class CommitCrawler:
     def __init__(self, repo_path: Path):
         self._repo = git.Repo(repo_path)
-        self._commit_fetcher = CommitFetcher()
+        self._commit_fetcher = CommitProvider()
 
         self._child_commit_graph = None
         self._latest_hashes = None
@@ -239,7 +241,8 @@ class CommitCrawler:
         return CrawlResult(self._child_commit_graph, current_paths_of_branches)
 
     def __get_latest_commits_of_branches(self):
-        return {self._repo.git.execute(f'git rev-parse "{branch}"').strip(): str(branch) for branch in self._repo.branches}        
+        return {self._repo.git.execute(f'git rev-parse "{branch}"').strip(): str(branch) for branch in
+                self._repo.branches}
 
     def __extract_child_tree(self, all_hashes):
         children = defaultdict(list)
@@ -247,14 +250,15 @@ class CommitCrawler:
             if i % 500 == 0:
                 print(f'{i}/{len(all_hashes)}')
             commit = self._repo.commit(hash)
-            for parent in commit.parents[:1]:  # When merging there is only a commit in the target branch, not the others that got merged in
+            # When merging there is only a commit in the target branch, not the others that got merged in
+            for parent in commit.parents[:1]:
                 children[parent.hexsha].append(self.__create_child_commit_entry(commit))
 
         initial_hash = all_hashes[-1]
         initial_commit = self._repo.commit(initial_hash)
         children['empty'].append(self.__create_child_commit_entry(initial_commit))
         return children
-    
+
     def __create_child_commit_entry(self, commit):
         return self._commit_fetcher.get_commit(commit)
 
@@ -263,7 +267,8 @@ class CommitCrawler:
         self.__follow_file_renames_from_commit('empty', current_paths_of_branches, bidict())
         return current_paths_of_branches
 
-    def __follow_file_renames_from_commit(self, commit_hash, current_paths_of_branches: CurrentFilePaths, branch_file_paths: bidict):
+    def __follow_file_renames_from_commit(self, commit_hash, current_paths_of_branches: CurrentFilePaths,
+                                          branch_file_paths: bidict):
         current_commit_hash = commit_hash
         while True:
             if current_commit_hash not in self._child_commit_graph:
@@ -272,16 +277,20 @@ class CommitCrawler:
             if number_child_commits == 0:
                 return
             if number_child_commits == 1:
-                current_commit_hash = self.__process_child_commmit(self._child_commit_graph[current_commit_hash][0], current_paths_of_branches, branch_file_paths)
+                current_commit_hash = self.__process_child_commmit(self._child_commit_graph[current_commit_hash][0],
+                                                                   current_paths_of_branches, branch_file_paths)
             else:
                 for child_commit in self._child_commit_graph[current_commit_hash]:
                     sub_branch_file_paths = copy(branch_file_paths)
-                    child_commit_hash = self.__process_child_commmit(child_commit, current_paths_of_branches, sub_branch_file_paths)
-                    self.__follow_file_renames_from_commit(child_commit_hash, current_paths_of_branches, sub_branch_file_paths)  # Only use recursion at commit graph branches
+                    child_commit_hash = self.__process_child_commmit(child_commit, current_paths_of_branches,
+                                                                     sub_branch_file_paths)
+                    self.__follow_file_renames_from_commit(child_commit_hash, current_paths_of_branches,
+                                                           sub_branch_file_paths)  # Only use recursion at commit graph branches
                 return
 
-    def __process_child_commmit(self, child_commit: Commit, current_paths_of_branches: CurrentFilePaths, branch_file_paths: Dict[str, str]):
-        for affected_file in child_commit._affected_files:
+    def __process_child_commmit(self, child_commit: Commit, current_paths_of_branches: CurrentFilePaths,
+                                branch_file_paths: bidict):
+        for affected_file in child_commit.affected_files:
             change_type = affected_file.change_type
             if change_type == 'A':
                 file_id = affected_file.file_id or str(uuid4())
@@ -291,7 +300,7 @@ class CommitCrawler:
             branch_file_paths[file_id] = affected_file.new_path
             if change_type == 'D':
                 del branch_file_paths[file_id]
-        child_commit_hash = child_commit._hash
+        child_commit_hash = child_commit.hash
         if child_commit_hash in self._latest_hashes:
             branch_name = self._latest_hashes[child_commit_hash]
             current_paths_of_branches.add_branch_paths(branch_name, dict(branch_file_paths))
@@ -301,5 +310,5 @@ class CommitCrawler:
 if __name__ == '__main__':
     crawler = CommitCrawler(get_repo_path())
     result = crawler.crawl()
-    result.write_to_db()    
+    result.write_to_db()
     print('Finished!')
