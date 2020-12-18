c752f4a6279c389e6a6bf95c1f4ea27ec02c67ac
Fabian <fabiannobis@gmx.de>
2020-12-18 22:15:53 +0100

    Add base crawl configuration capabilities to frontend. Maybe we shouldn't save it in the common store...

--- frontend/src/store/modules/common_store.js
+++ frontend/src/store/modules/common_store.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
-import { API_BASE_PATH } from '@/store/constants'
+import {API_BASE_PATH} from '@/store/constants'
 
 export const common_store = {
     state: {
         available_branches: [],
         current_branch: null,
+
+        is_crawling: false,
+        crawl_percentage: 0,
+        crawl_status_message: '',
+        crawl_error_message: '',
+
+        crawl_config: {},
     },
     mutations: {
         set_available_branches(state, branches) {
@@ -13,7 +20,45 @@ export const common_store = {
         },
         set_current_branch(state, branch) {
             state.current_branch = branch;
+        },
+        calculate_crawl_progress(state, status) {
+            switch (status.current_operation) {
+                case 'GET_PREVIOUS_COMMITS':
+                    state.is_crawling = true;
+                    state.crawl_percentage = 10;
+                    state.crawl_status_message = 'Caching previous data from database...';
+                    break;
+                case 'EXTRACT_COMMITS':
+                    state.is_crawling = true;
+                    state.crawl_percentage = 10 + 70 * status.commits_processed / status.commits_total;
+                    state.crawl_status_message = (`Processing commit ${status.commits_processed} `
+                        + `of ${status.commits_total}...`);
+                    break;
+                case 'CALCULATE_PATHS':
+                    state.is_crawling = true;
+                    state.crawl_percentage = 80
+                    state.crawl_status_message = 'Following file moves and renames...';
+                    break;
+                case 'SAVE_TO_DB':
+                    state.is_crawling = true;
+                    state.crawl_percentage = 90;
+                    state.crawl_status_message = 'Writing changes to database...';
+                    break;
+                case 'IDLE':
+                    state.is_crawling = false;
+                    state.crawl_percentage = 100;
+                    state.crawl_status_message = '';
+                    break;
+            }
+
+            state.crawl_error_message = status.error_message;
+            if (status.error_message)
+                state.is_crawling = false;
+        },
+        set_crawl_config(state, config) {
+            state.crawl_config = config;
         }
+
     },
 
     actions: {
@@ -23,5 +68,22 @@ export const common_store = {
             });
             return request;
         },
+        load_crawl_status(context) {
+            let request = axios.get(`${API_BASE_PATH}/crawl/status`).then(response => {
+                context.commit('calculate_crawl_progress', response.data);
+            });
+            return request;
+        },
+        load_crawl_config(context) {
+            let request = axios.get(`${API_BASE_PATH}/crawl/config`).then(response => {
+                context.commit('set_crawl_config', response.data);
+            });
+            return request;
+        },
+        save_crawl_config(context, config) {
+            context.commit('set_crawl_config', config)
+            let request = axios.put(`${API_BASE_PATH}/crawl/config`, config);
+            return request;
+        }
     }
 };
--- frontend/src/views/config/DatabaseUpdate.vue
+++ frontend/src/views/config/DatabaseUpdate.vue
@@ -1,11 +1,104 @@
 <template>
   <v-container fluid>
-    Some database update config
+    <v-row justify="center">
+      <v-col cols="12" md="8" xl="7">
+        <CardWithHelp
+            help_text=""
+            class="px-10 pb-5"
+        >
+          <v-card-title>Update Status</v-card-title>
+          <div>
+            <v-progress-linear
+                :value="$store.state.common.crawl_percentage"
+                :color="$store.state.common.is_crawling ? 'primary' : 'grey lighten-1'"
+            ></v-progress-linear>
+            <v-card-text>
+              {{ $store.state.common.crawl_status_message }}
+            </v-card-text>
+          </div>
+          <v-alert
+              type="error"
+              outlined
+              v-show="$store.state.common.crawl_error_message !== ''"
+          >
+            An error occured while updating:<br>
+            {{ $store.state.common.crawl_error_message }}
+          </v-alert>
+          <v-switch
+              v-model="crawl_config.update_before_crawl"
+              label="Fetch newest repo version before updating"
+          ></v-switch>
+          <v-text-field
+              v-model="crawl_config.webhook_token"
+              append-icon="mdi-dice-5-outline"
+              label="Webhook Token"
+              type="text"
+              @click:append="get_new_token"
+          ></v-text-field>
+            <DaysSelector
+              label="Only track branches with changes in the last"
+              :init_value="crawl_config.limit_tracked_branches_days_last_activity"
+              @change="crawl_config.limit_tracked_branches_days_last_activity = $event"
+            />
+          <v-btn
+              @click="trigger_update"
+              color="primary"
+              class="mr-5"
+              :disabled="$store.state.common.is_crawling"
+          >Save & Update now
+          </v-btn>
+          <v-btn
+              @click="save_config"
+          >Save
+          </v-btn>
+        </CardWithHelp>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+import {API_BASE_PATH} from '@/store/constants'
+
+import CardWithHelp from "@/components/commonComponents/CardWithHelp";
+import DaysSelector from "@/components/commonComponents/DaysSelector";
+
 export default {
-  name: "DatabaseUpdateConfig"
+  name: "DatabaseUpdateConfig",
+  components: {DaysSelector, CardWithHelp},
+  data() {
+    return {
+      crawl_config: {},
+      crawl_updater: null,
+    }
+  },
+  methods: {
+    trigger_update() {
+      this.save_config().then(() => {
+        axios.put(`${API_BASE_PATH}/crawl/update`);
+      });
+    },
+    save_config() {
+      return this.$store.dispatch('save_crawl_config', this.crawl_config);
+    },
+    get_new_token() {
+      axios.get(`${API_BASE_PATH}/crawl/random_token`).then(response => {
+        this.crawl_config.webhook_token = response.data;
+      });
+    }
+  },
+  mounted() {
+    this.crawl_updater = setInterval(() => {
+      this.$store.dispatch('load_crawl_status')
+    }, 1000);
+
+    this.$store.dispatch('load_crawl_config').then(() => {
+      this.crawl_config = this.$store.state.common.crawl_config;
+    });
+  },
+  beforeDestroy() {
+    clearInterval(this.crawl_updater);
+  }
 }
 </script>
