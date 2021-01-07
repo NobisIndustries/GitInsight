330d70bb8d40c84dfa1512b4a200ec18018f6f0f
Fabian <fabiannobis@gmx.de>
2021-01-07 19:16:03 +0100

    Dark mode improvements, part 2

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,7 +5,7 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     theme: {
-        dark: true,
+        dark: false,
         themes: {
             dark: {
                 primary: '#21CFF3',
